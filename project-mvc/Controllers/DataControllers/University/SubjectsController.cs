d8829a3d9ea85b88956adad6d38add18d407402f
pe6oto123 <pe6oto123@gmail.com>
2022-07-25 17:31:34 +0300

    Finished Subjects mvc/api

--- project-mvc/Controllers/DataControllers/University/SubjectsController.cs
+++ project-mvc/Controllers/DataControllers/University/SubjectsController.cs
@@ -1,164 +1,93 @@
-﻿/*using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using project_mvc.Database.Contexts;
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using project_mvc.ApiClient;
 using project_mvc.Database.Entities.University;
 
 namespace project_mvc.Controllers.DataControllers.University
 {
-    public class SubjectsController : Controller
-    {
-        private readonly DatabaseContext _context;
+	public class SubjectsController : Controller
+	{
+		private const string _apiRoute = "api/Subjects";
+		private HttpResponseMessage? _response;
 
-        public SubjectsController(DatabaseContext context)
-        {
-            _context = context;
-        }
+		// GET: Subjects
+		public async Task<IActionResult> Index(string subjectSearch)
+		{
+			ViewBag.SubjectSearch = subjectSearch;
 
-        // GET: Subjects
-        public async Task<IActionResult> Index()
-        {
-              return _context.Subject != null ? 
-                          View(await _context.Subject.ToListAsync()) :
-                          Problem("Entity set 'DatabaseContext.Subject'  is null.");
-        }
+			_response = await Client.GetClient().GetAsync($"{_apiRoute}?subjectSearch={subjectSearch}");
+			if (!_response.IsSuccessStatusCode)
+				return Problem(JObject.Parse(await _response.Content.ReadAsStringAsync()).ToString());
 
-        // GET: Subjects/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Subject == null)
-            {
-                return NotFound();
-            }
+			var subject = await _response.Content.ReadFromJsonAsync<IEnumerable<Subject>>();
 
-            var subject = await _context.Subject
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (subject == null)
-            {
-                return NotFound();
-            }
+			return View(subject);
+		}
 
-            return View(subject);
-        }
+		// GET: Subjects/Create
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        // GET: Subjects/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+		// POST: Subjects/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id,Name")] Subject subject)
+		{
+			if (ModelState.IsValid)
+			{
+				_response = await Client.GetClient().PostAsJsonAsync($"{_apiRoute}/", subject);
+				if (!_response.IsSuccessStatusCode)
+					return Problem(JObject.Parse(await _response.Content.ReadAsStringAsync()).ToString());
 
-        // POST: Subjects/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name")] Subject subject)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(subject);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(subject);
-        }
+				return RedirectToAction(nameof(Index));
+			}
+			return View(subject);
+		}
 
-        // GET: Subjects/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Subject == null)
-            {
-                return NotFound();
-            }
+		// GET: Subjects/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			_response = await Client.GetClient().GetAsync($"{_apiRoute}/{id}");
+			if (!_response.IsSuccessStatusCode)
+				return Problem(JObject.Parse(await _response.Content.ReadAsStringAsync()).ToString());
 
-            var subject = await _context.Subject.FindAsync(id);
-            if (subject == null)
-            {
-                return NotFound();
-            }
-            return View(subject);
-        }
+			var subject = await _response.Content.ReadFromJsonAsync<Subject>();
 
-        // POST: Subjects/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Subject subject)
-        {
-            if (id != subject.Id)
-            {
-                return NotFound();
-            }
+			return View(subject);
+		}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(subject);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!SubjectExists(subject.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(subject);
-        }
+		// POST: Subjects/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Subject subject)
+		{
+			if (ModelState.IsValid)
+			{
+				_response = await Client.GetClient().PutAsJsonAsync($"{_apiRoute}/{id}", subject);
+				if (!_response.IsSuccessStatusCode)
+					return Problem(JObject.Parse(await _response.Content.ReadAsStringAsync()).ToString());
 
-        // GET: Subjects/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Subject == null)
-            {
-                return NotFound();
-            }
+				return RedirectToAction(nameof(Index));
+			}
+			return View(subject);
+		}
 
-            var subject = await _context.Subject
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (subject == null)
-            {
-                return NotFound();
-            }
+		// POST: Subjects/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Delete(int id)
+		{
+			_response = await Client.GetClient().DeleteAsync($"{_apiRoute}/{id}");
+			if (!_response.IsSuccessStatusCode)
+				return Problem(JObject.Parse(await _response.Content.ReadAsStringAsync()).ToString());
 
-            return View(subject);
-        }
-
-        // POST: Subjects/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Subject == null)
-            {
-                return Problem("Entity set 'DatabaseContext.Subject'  is null.");
-            }
-            var subject = await _context.Subject.FindAsync(id);
-            if (subject != null)
-            {
-                _context.Subject.Remove(subject);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool SubjectExists(int id)
-        {
-          return (_context.Subject?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
-    }
+			return RedirectToAction(nameof(Index));
+		}
+	}
 }
-*/
